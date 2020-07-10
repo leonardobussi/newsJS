cf80f7d2d747a6191201ea763c20527c7447fa8e
leonardobussi <leonardobezerrabussy@gmail.com>
2020-07-09 22:00:00 -0300

    segundo commit, me respeita menó

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const app = require('./config/server');
 
-const rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
+const rotaNoticias = require('./app/routes/noticias')(app);
 
-const rotaHome = require('./app/routes/home');
-rotaHome(app);
+const rotaHome = require('./app/routes/home')(app);
 
-const rotaFormulario = require('./app/routes/formulario_inclisao_noticia');
-rotaFormulario(app);
+const rotaFormulario = require('./app/routes/formulario_inclisao_noticia')(app);
+
 
 app.listen(3000, function(){
     console.log("servidor rodando com express")
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,21 @@
 module.exports =  function(app) {
+
+    app.get('/noticias', function(req, res){
+
+    const mysql = require('mysql');
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '0000',
+        database: 'portal_noticias'
+    });
+
+    connection.query('select * from noticias', function(error, result){
+         res.send(result);
+        });
    
-app.get('/noticias', function(req, res){
-    res.render('noticias/noticias')
+
+    //res.render('noticias/noticias')
 }); 
 }
