7227f1560a5f6f19b2683f178bcef5f2d07e782e
leonardobussi <leonardobezerrabussy@gmail.com>
2020-07-11 17:14:06 -0300

    rafactoring the project - express-validate

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,7 +7,19 @@ application.get('/formulario_inclusao_noticia', function(req, res){
 application.post('/noticias/salvar', function(req, res){
     const noticia = req.body;
     
+    req.assert('titulo','o titulo é obrigatorio').notEmpty();
+    req.assert('resumo','o resumo é obrigatorio').notEmpty();
+    req.assert('resumo','o resumo tem que ter entre 10 a 100 caracteres').len(10, 100);
+    req.assert('autor','o autor é obrigatorio').notEmpty();
+    req.assert('data_noticia','o data é obrigatorio').notEmpty().isDate({format: 'YYYY-MM-DD'});
+    req.assert('noticia','o campo de noticia é obrigatorio').notEmpty();
 
+    const erros = req.validationErrors();
+
+    if(erros){
+        res.render('admin/form_add_noticia')
+        return;
+    }
 
     const connection = application.config.dbConnection();
     const noticiasModel = new application.app.models.NoticiasDAO(connection);
