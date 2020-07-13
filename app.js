cbc8a4c26b4e3099350bc050607799e9ec59970b
leonardobussi <leonardobezerrabussy@gmail.com>
2020-07-13 09:46:25 -0300

    insert data of back end for front end

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 const app = require('./config/server');
 
-// const rotaNoticias = require('./app/routes/noticias')(app);
-
-// const rotaHome = require('./app/routes/home')(app);
-
-// const rotaFormulario = require('./app/routes/formulario_inclisao_noticia')(app);
-
-
 app.listen(3000, function(){
     console.log("aplcação está fincionado!")
     console.log('----------------------------')
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,10 @@
 module.exports.index = function (application, req, res){
-    res.render('home/index');
+
+    const connection = application.config.dbConnection();
+    const noticiasModel = new application.app.models.NoticiasDAO(connection); 
+
+    noticiasModel.get5UltimasNoticias(function(error, result){
+        res.render('home/index', {noticias: result});
+    });
+    
 }
